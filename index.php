e17516c955e20bdad6f547c433b6d641eeb60496
xxxxbear <995280199@qq.com>
2016-11-18 16:21:38 +0800

    添加怎么删除

--- index.php
+++ index.php
@@ -6,3 +6,15 @@ git add .
 git commit -m "注释语句"
 git pull origin master
 git push -u origin master
+
+
+
+
+删除
+
+git rm -r --cached .
+git add .
+git commit -m ".gitignore is now working"
+
+
+To undo git rm --cached filename, use git add filename.
